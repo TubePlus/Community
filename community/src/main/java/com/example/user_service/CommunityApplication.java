0bbbfbefaf01eb9103dc8af7dc9ac1b33ec2aca5
goeom77 <7eom14@gmail.com>
2023-10-10 16:02:27 +0900

    Feat base setting

---
+++ community/src/main/java/com/example/user_service/CommunityApplication.java
@@ -0,0 +1,15 @@
+package com.example.user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing // base entity 자동 적용
+@SpringBootApplication
+public class CommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommunityApplication.class, args);
+	}
+
+}
