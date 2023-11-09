e38c186b3cf1c7d193a7243ea09275ecfbbadc45
goeom77 <7eom14@gmail.com>
2023-11-09 12:02:33 +0900

    Test kafka producer test

---
+++ community/src/main/java/com/example/community_service/config/kafka/KafkaProducerConfig.java
@@ -0,0 +1,38 @@
+package com.example.community_service.config.kafka;
+
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Configuration
+public class KafkaProducerConfig {
+    @Bean
+    public ProducerFactory<String, String> producerFactory(){
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"); // kafka container host
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);// key serializer
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class); // value serializer
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+    // test bean 생성 로직(테스트용)
+    // input(topicName, partition, replication)
+    @Bean
+    public NewTopic topicTest() {
+        return new NewTopic("test",1, (short) 1);
+    }
+
+    @Bean
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
